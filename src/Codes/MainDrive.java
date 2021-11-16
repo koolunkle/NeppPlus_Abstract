f209e4ad8ed941bd1edaea79d532f5de6faf5ce7
김준기 <gugi329@daum.net>
2021-11-16 10:28:17 +0900

    MainDrive 클래스 작성 -> 추상화(Abstract) 결과 확인

--- src/Codes/MainDrive.java
+++ src/Codes/MainDrive.java
@@ -4,6 +4,14 @@ public class MainDrive {
 
 	public static void main(String[] args) {
 
+		Dog dog1 = new Dog();
+		Cat cat1 = new Cat();
+
+//		Animal ani1 = new Animal();
+
+		dog1.bark();
+		cat1.bark();
+
 	}
 
 }
